f98a4b8fba726e8bba04200dcc51526e56606cea
Harsh Patel <harshp5@usf.edu>
2024-09-21 18:13:20 -0400

    Initialize inventory with product objects

---
+++ order_management_system.js
@@ -0,0 +1,9 @@
+// Task 1 Create an Inventory Array of Product Objects
+let inventory = [
+    {name: "Cold Brew" , price: 5, quantity: 12},
+    {name: "Energy Drink" , price: 3, quantity: 15},
+    {name: "Dark Roast" , price: 6, quantity: 10},
+    {name: "Light Roast" , price: 4, quantity: 5},
+    {name: "Mug" , price: 10, quantity: 7}
+];
+
