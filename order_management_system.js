0ea9a64c739abf00ab68a1cdc61b3453f0ae396e
Harsh Patel <harshp5@usf.edu>
2024-09-22 15:44:57 -0400

    Create placeOrder function

--- order_management_system.js
+++ order_management_system.js
@@ -6,6 +6,7 @@ let inventory = [
     {name: "Light Roast" , price: 4, quantity: 5},
     {name: "Mug" , price: 10, quantity: 7}
 ];
+console.log(inventory);
 
 // Task 2 Create an Orders Array of Order Objects
 let orders = [];
@@ -13,8 +14,46 @@ let orders = [];
 
 // Task 3 Create a Function to Place an Order
 
-function placeOrder(orders)
-orders.push({
-    customerName: "Daniel Wig"
+function placeOrder(customerName,itemsOrdered) {   
+    itemsOrdered.forEach(item => {
+        let product = inventory.find(p => p.name === item.name);
+        //to check if product is in stock and has enough quantity
+       if (!product.name || product.name < product.quantity)
+            return;
+});
+    if (inventory.quantity <= 0) {
+         console.log(`Error: item not in stock :( `);
+        return;
+    }
 
-})
+itemsOrdered.forEach(item => {
+    let product = inventory.find(p => p.name === item.name);
+    product.quantity -= item.quantity;
+});
+
+   
+   // for (let quantity in itemsOrdered) {
+   //     inventory.find(item => itemsOrdered.name === item.name);
+     //   if (!itemsOrdered) {
+       //     console.log(`Error: Item doesn't exist`);
+         //  break;
+       //}
+      //if(itemsOrdered.quantity > inventory.quantity) {
+        //    console.log(`Error: No items in stock`);
+          //  break;
+      // }
+   // }
+   // for (let quantity in itemsOrdered) {
+   //     inventory.find(item = itemsOrdered.name === item.name);
+     //  itemsOrdered.quantity -= inventory.quantity;
+   // }
+
+let newOrder = {
+    customerName: customerName,
+    itemsOrdered: itemsOrdered,
+    status: "Pending"
+};
+orders.push(newOrder);
+}
+console.log(placeOrder("Jake Bag", {name:"Energy Drink", quantity: 2}));
+console.log(`Order has been placed for ${customerName}`, newOrder);
